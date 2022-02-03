8b0cdb3f6d9d6ac9d6ed587116f1424a5ee85d86
Eric Hanoun <hanoune@sheridancollege.ca>
2022-02-03 11:41:37 -0500

    Setting up the repository

---
+++ src/inclassex01/InClassEx01.java
@@ -0,0 +1,30 @@
+package inclassex01;
+
+
+public class InClassEx01 {
+    
+    public static void main(String[] args) {
+        
+        int[] temperatures = new int[14];
+        for(int i = 0; i < temperatures.length; i++){
+            temperatures[i] = (int)(Math.random() * 201) - 100;
+        }
+        
+        for(int temps: temperatures){
+            System.out.println(temps);
+        }
+        
+        System.out.println("Average temperature: " + calcAvg(temperatures));
+    }
+    
+    public static double calcAvg(int[] temps){
+        double avg = 0;
+        
+        for(int t: temps){
+            if (t > -50 || t < 50) {
+                avg += t;
+            }   
+        } 
+        return (double)avg/temps.length;
+    }
+}
